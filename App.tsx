ba0938163883c15722e49db3e5520eca585082e4
Fleek <fleek@Fleeks-MacBook-Pro-2.local>
2023-04-20 21:11:49 +0530

    updated application

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react';
-import { Provider as ReduxProvider, useDispatch, useSelector } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import { LogBox } from 'react-native';
 
 import { ThemeProvider, WrapperContainer } from './src/components';
 import { unregister, checkTheme } from './src/utils';
 import store from './src/redux/store';
 import Routes from './src/routes/routes';
-
-import { getRequest } from './src/services/api';
-import { GET_POPULAR_LIST } from './src/services/urls';
-import { getToken } from './src/redux/actions/auth';
-
 
 const App = () => {
 
